bc3722f7ebd176d7c85f756b07bdd5fbac189728
Jarek Kruza <jkruza@gmail.com>
2023-03-19 14:50:30 +0100

    wip

---
+++ index.php
@@ -0,0 +1,47 @@
+<?php
+
+$dbhost="localhost";
+$dbuser="username";
+$dbpass="password";
+$dbname="database";
+
+
+// include config.php if exists
+if (file_exists('config.php')) {
+    include 'config.php';
+}
+
+// Connect to mysql database using PDO
+$pdo = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbuser, $dbpass);
+
+//get guid from GET
+$guid = $_GET['token'];
+
+// selct from tokens where token = $guid
+$sql = "SELECT * FROM tokens WHERE token = :guid";
+$stmt = $pdo->prepare($sql);
+$stmt->execute(array(':guid' => $guid));
+
+//check if token exists
+if ($stmt->rowCount() == 0) {
+    // return 500 http error
+    http_response_code(500);
+    exit;
+}
+
+// get host, data_key and data_value from GET
+$host = $_GET['host'];
+$data_key = $_GET['data_key'];
+$data_value = $_GET['data_value'];
+
+// insert into monitor-data (host, data_key, data_value) values ($host, $data_key, $data_value)
+$sql = "INSERT INTO `monitor-data` (host, data_key, data_value) VALUES (:host, :data_key, :data_value)";
+$stmt = $pdo->prepare($sql);
+$stmt->execute(array(':host' => $host, ':data_key' => $data_key, ':data_value' => $data_value));
+
+// return 200 http success
+http_response_code(200);
+
+
+
+?>
